f3ec1861601e0df057337a7e40a114baa8006a95
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-08-26 08:43:54 +0530

    terraform msg

--- 1.ec2/ec2.tf
+++ 1.ec2/ec2.tf
@@ -1,16 +1,13 @@
-
-
-
-resource "aws_security_group" "allow_ssh" {
+resource "aws_security_group" "allow_ssh" { //Here 'allow_ssh' is terraform resource name
   name        = "allow_ssh"
   description = "allowing SSH Access"
 
 
     ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port        = 0  #22
+    to_port          = 0 # 22
+    protocol         = "tcp" #Transmission Control Protocol (TCP)
+    cidr_blocks      = ["0.0.0.0/0"] #Classless Inter-Domain Routing 
     }
 
   egress {
@@ -20,19 +17,19 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  tags = {
+  tags = { #Here 'allow_ssh','Lingaiah' is the ec2-instance name in AWS, not related to Terrafrom.
     Name = "allow_ssh"
     Createdby="Lingaiah"
   }
 
 }
 
-resource "aws_instance" "db" {
+resource "aws_instance" "db" { //Here 'db' is terraform resource name
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id] #its is a list type declared in this way [ ]
 
   tags = {
-    Name = "HelloWorld-db"
+    Name = "HelloWorld Docker" #Here 'Helloworld-db' is the ec2-instance name in AWS, not related to Terrafrom.
   }
 }
